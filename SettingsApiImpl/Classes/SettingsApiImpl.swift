77dd7fd0f844693295c59123863c5892f1c49f8f
morapelker <morapelker@gmail.com>
2022-07-15 14:01:12 +0300

    "v1.0.6"

--- SettingsApiImpl/Classes/SettingsApiImpl.swift
+++ SettingsApiImpl/Classes/SettingsApiImpl.swift
@@ -27,20 +27,26 @@ public class SettingsApiImpl: SettingsApi {
     }
     
     public func updateAvatar(avatar: String?) -> AnyPublisher<Any?, Error> {
-        print("update avatar", avatar)
-        return Just(nil).delay(for: 1.0, scheduler: DispatchQueue.main).setFailureType(to: Error.self).eraseToAnyPublisher()
+        return api.requestRx(request: .init(path: "users/current", withAuth: true, method: .patch), parameters: [
+            "avatar": avatar ?? ""
+        ]).mapError({_ in NSError(domain: "Could not complete this request", code: 1)}).eraseToAnyPublisher()
+//        print("update avatar", avatar)
+//        return Just(nil).delay(for: 1.0, scheduler: DispatchQueue.main).setFailureType(to: Error.self).eraseToAnyPublisher()
     }
     
     public func updateEmail(toEmail: String) -> AnyPublisher<Any?, Error> {
-        print("update email", toEmail)
-        return Just(nil).delay(for: 1.0, scheduler: DispatchQueue.main).setFailureType(to: Error.self).eraseToAnyPublisher()
+        return api.requestRx(request: .init(path: "users/current", withAuth: true, method: .patch), parameters: [
+            "email": toEmail
+        ]).mapError({_ in NSError(domain: "Could not complete this request", code: 1)}).eraseToAnyPublisher()
+//        print("update email", toEmail)
+//        return Just(nil).delay(for: 1.0, scheduler: DispatchQueue.main).setFailureType(to: Error.self).eraseToAnyPublisher()
     }
     
    
-    public func updatePassword(oldPassword: String, newPassword: String) -> AnyPublisher<String, NSError> {
+    public func updatePassword(oldPassword: String, newPassword: String) -> AnyPublisher<Any?, NSError> {
         print("update password", oldPassword, newPassword)
 //        return Fail(error: NSError(domain: "Invalid password", code: 1)).delay(for: 1.0, scheduler: DispatchQueue.main).eraseToAnyPublisher()
-        return Just("new token").delay(for: 1.0, scheduler: DispatchQueue.main).setFailureType(to: NSError.self).eraseToAnyPublisher()
+        return Just(nil).delay(for: 1.0, scheduler: DispatchQueue.main).setFailureType(to: NSError.self).eraseToAnyPublisher()
     }
     
     
